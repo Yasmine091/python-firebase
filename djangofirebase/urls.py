bc68064802783f22217bc547f66ba05eb4fe1cf5
Yasmine091 <yasmine.emdounan@gmail.com>
2021-08-23 17:02:29 +0200

    Templates + config url

--- djangofirebase/urls.py
+++ djangofirebase/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.conf.urls import url
+from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$',views.singIn),
+    url(r'^postsign/',views.postsign),
 ]
