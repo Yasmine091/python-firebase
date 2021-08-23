9984b0fb926cc5a7708ec8e3cd03fce4eee1784a
Yasmine091 <yasmine.emdounan@gmail.com>
2021-08-23 16:54:55 +0200

    Installation django-admin

--- manage.py
+++ manage.py
@@ -3,12 +3,10 @@
 import os
 import sys
 
-# Default port
-from django.core.management.commands.runserver import Command as runserver
-runserver.default_port = "7090"
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangofirebase.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Firebase.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
