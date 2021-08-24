322e716c452bd7a0b0ac1bbcf31d21afeba98e55
Yasmine091 <yasmine.emdounan@gmail.com>
2021-08-24 14:01:53 +0200

    Config postgres heroku

--- djangofirebase/settings.py
+++ djangofirebase/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'djangofirebase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
